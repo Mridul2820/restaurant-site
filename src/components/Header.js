2e28d3577b07c3ff1d48f7eadc1ff5e4d016fe6f
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-16 21:37:58 +0530

    haeder

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div id="main">
+            <div className="header-heading">
+                <h3>Its Burger Time 🍔</h3>
+                <h1>
+                    <span>Burger</span> for 
+                    <br /> Week
+                </h1>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <div className="header-btns">
+                    <a href="#" className="header-btn">Order</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
